2c432be5c4484f5e6254d01ae4729570c1010e32
Kalu Kalu <kalu@Kalus-MBP.attlocal.net>
2025-02-03 18:44:03 -0800

    Add user-friendly file upload form and improve API documentation

--- pdf_compressor/views.py
+++ pdf_compressor/views.py
@@ -1,12 +1,41 @@
 from rest_framework import status
 from rest_framework.views import APIView
 from rest_framework.response import Response
+from rest_framework.parsers import MultiPartParser, FormParser
+from rest_framework.renderers import TemplateHTMLRenderer, JSONRenderer
 from .serializers import PDFFileSerializer
 from .utils import compress_pdf
 import magic
 
 class PDFCompressorView(APIView):
+    """
+    API endpoint that allows PDF files to be uploaded and compressed.
+    """
+    parser_classes = (MultiPartParser, FormParser)
+    renderer_classes = [JSONRenderer, TemplateHTMLRenderer]
+    template_name = 'pdf_compressor/index.html'
+    
+    def get(self, request, *args, **kwargs):
+        """
+        Renders the file upload form.
+        """
+        serializer = PDFFileSerializer()
+        return Response({
+            'serializer': serializer,
+            'title': 'PDF Compression API',
+            'description': 'Upload a PDF file to compress it.'
+        }, template_name='pdf_compressor/index.html')
+
     def post(self, request):
+        """
+        Handles PDF file upload and compression.
+        
+        Parameters:
+        - pdf_file: The PDF file to compress (required)
+        
+        Returns:
+        - Compressed PDF file as attachment
+        """
         serializer = PDFFileSerializer(data=request.data)
         if serializer.is_valid():
             pdf_file = request.FILES['pdf_file']
@@ -36,4 +65,8 @@ class PDFCompressorView(APIView):
                     status=status.HTTP_500_INTERNAL_SERVER_ERROR
                 )
         
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+        return Response(
+            serializer.errors,
+            status=status.HTTP_400_BAD_REQUEST,
+            template_name='pdf_compressor/index.html'
+        )
