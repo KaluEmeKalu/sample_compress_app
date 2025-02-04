5b38d643fbb4e041a12a98787fb5e091f131980b
Kalu Kalu <kalu@Kalus-MBP.attlocal.net>
2025-02-03 18:34:08 -0800

    Update URLs to handle root redirect and static files

--- summit_project/urls.py
+++ summit_project/urls.py
@@ -1,7 +1,11 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
+from django.views.generic import RedirectView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('pdf_compressor.urls')),
-]
+    path('', RedirectView.as_view(url='/api/', permanent=False)),  # Redirect root to API
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)  # Serve static files
